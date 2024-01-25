411914ccfbb752b30f82ebf4401be9f265cac0bb
eoussama <oussama.essamadi@gmail.com>
2024-01-25 22:45:57 +0100

    feat: error catching, #17

--- src/lib/core/helpers/auth.helper.ts
+++ src/lib/core/helpers/auth.helper.ts
@@ -8,17 +8,19 @@ import { FirebaseHelper } from './firebase.helper';
 export class AuthHelper {
 
   static login(credentials: TFirebaseConfig): Promise<string> {
-    return new Promise(resolve => {
+    return new Promise((resolve, reject) => {
       const auth = FirebaseHelper.getAuth(credentials);
       if (!auth) throw new InvalidAppError();
 
       const provider = FirebaseHelper.getProvider();
       if (!provider) throw new InvalidProviderError('unknown');
 
-      signInWithPopup(auth, provider).then((e: any) => {
-        const token = (e as TToken)._tokenResponse.oauthIdToken ?? '';
-        resolve(token);
-      })
+      signInWithPopup(auth, provider)
+        .then((e: any) => {
+          const token = (e as TToken)._tokenResponse.oauthIdToken ?? '';
+          resolve(token);
+        })
+        .catch(reject);
     });
   }
 }
