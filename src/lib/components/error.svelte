ac4ca3870ad9566a09719a486c46d695c92f515d
eoussama <oussama.essamadi@gmail.com>
2024-01-27 15:12:00 +0100

    feat: error component, #21

---
+++ src/lib/components/error.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let errorMsg: string;
+</script>
+
+<div class="error">{errorMsg}</div>
+
+<style lang="scss">
+	.error {
+		font-size: 14px;
+		color: var(--color-error);
+		font-weight: var(--font-weight-bold);
+	}
+</style>
