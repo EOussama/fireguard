244129472d113aa33774ab97e1ba3e598acd9b30
eoussama <oussama.essamadi@gmail.com>
2024-02-02 16:16:34 +0100

    feat: success page, #31

--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -1,1 +1,28 @@
-success
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { appStore } from '$lib/core/stores/app.store';
+	import { EventHelper, EventType } from '@eoussama/firemitt';
+
+	let iterations = 3;
+	const interval = 1000;
+
+	$: remainingTime = `${iterations} second${iterations === 1 ? '' : 's'}`;
+
+	const onClose = (): void => {
+		EventHelper.send(EventType.AuthSucceded, { token: $appStore.token });
+		window.close();
+	};
+
+	onMount(() => {
+		const intervalFn = setInterval(() => {
+			if (--iterations === 0) {
+				onClose();
+				clearInterval(intervalFn);
+			}
+		}, interval);
+	});
+</script>
+
+Auto closing in {remainingTime}...
+<br />
+<button on:click={onClose}>Close</button>
