645320266f3cf87112d818d1480973ee7d81b113
eoussama <oussama.essamadi@gmail.com>
2024-02-04 23:32:39 +0100

    feat: waves component, #34

---
+++ src/lib/components/waves.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		console.log({ canvas });
+	});
+</script>
+
+<div class="waves">
+	<canvas bind:this={canvas} class="waves__canvas"></canvas>
+</div>
+
+<style lang="scss">
+	.waves {
+		width: 100%;
+		height: 100%;
+
+		&__canvas {
+			width: 100%;
+			height: 100%;
+		}
+	}
+</style>
