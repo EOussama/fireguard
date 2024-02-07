7fa6549497c24bc5b2816323b5a344859a8c4de4
eoussama <oussama.essamadi@gmail.com>
2024-02-07 23:19:56 +0100

    feat: screw this shit, #31

---
+++ src/lib/core/enums/auth-status.enum.ts
@@ -0,0 +1,5 @@
+export enum AuthStatus {
+  Pending,
+  Success,
+  Failure
+}
--- src/routes/success/+page.svelte
+++ src/routes/success/+page.svelte
@@ -1,10 +1,8 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import { EventHelper, EventType } from '@eoussama/firemitt';
+	import { EventHelper, EventType } from '@eoussama/firemitt'; 
 
-	import Waves from '$lib/components/waves.svelte';
 	import { appStore } from '$lib/core/stores/app.store';
-
 	import { FireguardHelper } from '$lib/core/helpers/fireguard.helper';
 
 	let iterations = 3;
@@ -33,4 +31,3 @@
 Auto closing in {remainingTime}...
 <br />
 <button on:click={onClose}>Close</button>
-<Waves />
