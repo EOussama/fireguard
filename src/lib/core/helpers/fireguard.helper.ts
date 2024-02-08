e9118fd7a4a5d3ec732c1f112bd871edc9224012
eoussama <oussama.essamadi@gmail.com>
2024-02-08 13:55:20 +0100

    feat: helper docs, #30

--- src/lib/core/helpers/fireguard.helper.ts
+++ src/lib/core/helpers/fireguard.helper.ts
@@ -1,4 +1,15 @@
+/**
+ * @description
+ * Helper class for Fireguard related functionalities.
+ */
 export class FireguardHelper {
+
+  /**
+   * @description
+   * Checks if Fireguard is ready.
+   *
+   * @returns Returns true if Fireguard is ready, otherwise false.
+   */
   static isReady(): boolean {
     return Boolean(globalThis.window.opener);
   }
