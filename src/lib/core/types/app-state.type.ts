79270ba6a06d2af30f06bb8a1d95cc79dc7dc8ff
eoussama <oussama.essamadi@gmail.com>
2024-02-08 13:48:07 +0100

    feat: type docs, #30

--- src/lib/core/types/app-state.type.ts
+++ src/lib/core/types/app-state.type.ts
@@ -1,8 +1,34 @@
 import type { TFireguardConfig, TNullable } from '@eoussama/firemitt';
 
+
+
+/**
+ * @description
+ * Represents the state of the application.
+ */
 export type TAppState = {
+
+  /**
+   * @description
+   * Indicates whether the application is loading.
+   */
   loading: boolean
+
+  /**
+   * @description
+   * The authentication token. Can be null.
+   */
   token: TNullable<string>
+
+  /**
+   * @description
+   * Error message if any. Can be null.
+   */
   errorMsg: TNullable<string>
+
+  /**
+   * @description
+   * The application configuration. Can be null.
+   */
   config: TNullable<TFireguardConfig>
 }
