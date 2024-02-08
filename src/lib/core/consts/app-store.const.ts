6604c46b9f6971fe01ae39d966b3ceb2bb0261c5
eoussama <oussama.essamadi@gmail.com>
2024-02-08 13:44:10 +0100

    feat: enum docs, #30

--- src/lib/core/consts/app-store.const.ts
+++ src/lib/core/consts/app-store.const.ts
@@ -1,8 +1,34 @@
 import type { TAppState } from '../types/app-state.type';
 
+
+
+/**
+ * @description
+ * Represents the initial state of the application.
+ */
 export const initialAppState: TAppState = {
+
+  /**
+   * @description
+   * The authentication token. Initialized to null.
+   */
   token: null,
+
+  /**
+   * @description
+   * The application configuration. Initialized to null.
+   */
   config: null,
+
+  /**
+   * @description
+   * Error message if any. Initialized to null.
+   */
   errorMsg: null,
+
+  /**
+   * @description
+   * Indicates whether the application is loading. Initialized to true.
+   */
   loading: true
 };
