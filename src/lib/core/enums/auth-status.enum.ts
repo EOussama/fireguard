36d67357faf1acecd7803e0a9f13acefec7105c5
eoussama <oussama.essamadi@gmail.com>
2024-02-08 13:44:15 +0100

    feat: enum docs, #30

--- src/lib/core/enums/auth-status.enum.ts
+++ src/lib/core/enums/auth-status.enum.ts
@@ -1,5 +1,25 @@
+/**
+ * @description
+ * Enum representing the authentication status.
+ */
 export enum AuthStatus {
+
+  /**
+   * @description
+   * Pending authentication status.
+   * This is the default state.
+   */
   Pending,
+
+  /**
+   * @description
+   * Successful authentication status.
+   */
   Success,
+
+  /**
+   * @description
+   * Failed authentication status.
+   */
   Failure
 }
