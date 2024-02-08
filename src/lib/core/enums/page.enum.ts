1d9f093d9c193e5aa7dda682f907a152b64d73d7
eoussama <oussama.essamadi@gmail.com>
2024-02-08 13:44:15 +0100

    feat: enum docs, #30

--- src/lib/core/enums/page.enum.ts
+++ src/lib/core/enums/page.enum.ts
@@ -1,5 +1,24 @@
+/**
+ * @description
+ * Enum representing different pages in the application.
+ */
 export enum Page {
+
+  /**
+   * @description
+   * Represents the index page.
+   */
   Index = '/',
+
+  /**
+   * @description
+   * Represents the success page.
+   */
   Success = '/success',
+
+  /**
+   * @description
+   * Represents the failure page.
+   */
   Failure = '/failure'
 }
