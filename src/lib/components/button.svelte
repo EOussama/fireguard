4441d98c1021728767eaf09757599638c68e3c7a
eoussama <oussama.essamadi@gmail.com>
2024-02-12 15:54:29 +0100

    feat: button component, #51

---
+++ src/lib/components/button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+
+	/**
+	 * @description
+	 * The label of the button
+	 */
+	export let label: string = 'Click Me!';
+
+	/**
+	 * @description
+	 * Event dispatcher
+	 */
+	const dispatch = createEventDispatcher();
+
+	/**
+	 * @description
+	 * Button click event
+	 *
+	 * @param e Event object
+	 */
+	const onClick = (e: MouseEvent) => dispatch('click', { e });
+</script>
+
+<button class="btn" on:click={onClick}>{label}</button>
+
+<style lang="scss">
+	.btn {
+		cursor: pointer;
+
+		border: none;
+		border-radius: 5px;
+		padding: 10px 20px;
+
+		transition-duration: 0.2s;
+		transition-property: background-color;
+
+		color: var(--color-text);
+		font-family: var(--font-primary);
+		background-color: rgba(var(--color-primary-rgb), 0.2);
+
+		&:hover {
+			background-color: rgba(var(--color-primary-rgb), 0.5);
+		}
+	}
+</style>
