2bd75cc83f5b402374ad893f3550d1ec03de9ef4
eoussama <oussama.essamadi@gmail.com>
2024-02-12 15:54:42 +0100

    feat: close and navigate helper functions, #51

--- src/lib/core/helpers/fireguard.helper.ts
+++ src/lib/core/helpers/fireguard.helper.ts
@@ -1,3 +1,10 @@
+import { base } from '$app/paths';
+import { goto } from '$app/navigation';
+
+import type { Page } from '../enums/page.enum';
+
+
+
 /**
  * @description
  * Helper class for Fireguard related functionalities.
@@ -12,5 +19,23 @@ export class FireguardHelper {
    */
   static isReady(): boolean {
     return Boolean(globalThis.window.opener);
+  }
+
+  /**
+   * @description
+   * Closes Fireguard
+   */
+  static close(): void {
+    window.close();
+  }
+
+  /**
+   * @description
+   * Navigates to specified page
+   *
+   * @param page The page to navigate to
+   */
+  static navigate(page: Page): void {
+    goto(`${base}/${page}`);
   }
 }
